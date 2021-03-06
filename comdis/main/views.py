0aedbd15340c1d5175b9942a06f95c83887f4648
ricky <rickyrug@gmail.com>
2021-03-06 17:20:50 -0600

    adding templates

--- comdis/main/views.py
+++ comdis/main/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render("hi")
+    context = {}
+    return render(request,"main/index/index.html",context)
